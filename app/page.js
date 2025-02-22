4cf1254b5f4abc788d0d0317476a4a03e8147b23
NuntichaThummee <nunticha.ppalaloy909@gmail.com>
2025-02-22 02:51:08 -0500

    styling home page

--- app/page.js
+++ app/page.js
@@ -1,27 +1,28 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className={styles.background}>
-      <div className={styles.home}>
-      <main className={styles.logo}>
-        <Image
-          className={styles.logo}
-          src="/CartBuddyLogo.png"
-          alt="Cart Buddy Logo"
-          width={540}
-          height={114}
-          priority
-        />
+      <div className={styles.background}>
+        <div className={styles.home}>
+          <main className={styles.logo}>
+            <Image
+              className={styles.logo}
+              src="/CartBuddyLogo.png"
+              alt="Cart Buddy Logo"
+              width={540}
+              height={114}
+              priority
+            />
 
-        <div className={styles.card}>
-          <h1>Welcome</h1>
-          <h3>Let Cart Buddy Make Your Shopping easier!</h3>
+            <div className={styles.card}>
+              <h1>Welcome</h1>
+              <h3>Let Cart Buddy Make Your Shopping easier!</h3>
+            </div>
+
+          </main>
         </div>
-
-      </main>
-    </div>
-    </div >
+      </div >
   );
 }
